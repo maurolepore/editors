5d13712dcb2f1fb9b3dc30ef86edb18d21941c4d
Mauro Lepore <maurolepore@gmail.com>
2023-03-18 14:11:34 +0000

    Label standard-editors output

--- create_readme.sh
+++ create_readme.sh
@@ -5,6 +5,7 @@ echo                                                     >> $file
 echo 'Updated on '$(date)                                >> $file
 echo                                                     >> $file
 echo '```bash'                                           >> $file
+echo # Standard editors                                  >> $file
 cat out_standard.txt | sort | column -t                  >> $file
 echo '```'                                               >> $file
 
