3a59341e8dc566dda512fc09e57921d2da2d4528
Mauro Lepore <maurolepore@gmail.com>
2023-03-18 10:59:01 -0300

    Stand alone output (#2)
    * Use rstudio
    
    * Pass editors list as an argument
    
    * Store output stand-alone

--- last_activity_on_assigned_issues.sh
+++ last_activity_on_assigned_issues.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-for i in $(cat editors)
+file=${1:-editors}
+for i in $(cat $file)
 do
   gh issue list --limit 1 -S "assignee:$i state:open state:closed sort:updated" \
     --repo ropensci/software-review \
     --json state,updatedAt,number,assignees --template \
-    '{{range .}}{{tablerow .state .updatedAt (printf "#%v" .number) (pluck "login" .assignees | join ",") (timeago .updatedAt)}}{{end}}' | cat  
+    '{{range .}}{{tablerow .state .updatedAt (printf "#%v" .number) (pluck "login" .assignees | join ",") (timeago .updatedAt)}}{{end}}' | cat
 done
