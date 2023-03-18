a8bd4b837c2f230cf1b702e75289934fc416ab35
Mauro Lepore <maurolepore@gmail.com>
2023-03-18 14:39:43 +0000

    Fix create_readme.sh to show stats output

--- create_readme.sh
+++ create_readme.sh
@@ -5,10 +5,10 @@ echo                                                     >> $file
 echo 'Updated on '$(date)                                >> $file
 echo                                                     >> $file
 echo '```bash'                                           >> $file
-echo # Standard                                          >> $file
+echo '# Standard'                                        >> $file
 cat out_standard.txt | sort | column -t                  >> $file
 echo                                                     >> $file
-echo # Stats                                             >> $file
-cat out_standard.txt | sort | column -t                  >> $file
+echo '# Stats'                                           >> $file
+cat out_stats.txt | sort | column -t                     >> $file
 echo '```'                                               >> $file
 
